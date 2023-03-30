249558ec2f623e01a9f0cb379cb48040cf1802e9
washtakzo <washtakzo@gmail.com>
2023-03-30 19:51:06 +0200

    UI : HomeScreen - add ImageBackgound & LinearGradient

--- App.tsx
+++ App.tsx
@@ -1,18 +1,28 @@
-import { View, StyleSheet, SafeAreaView } from "react-native";
+import { View, StyleSheet, ImageBackground } from "react-native";
 import HomeScreen from "./pages/HomeScreen";
 import Colors from "./helper/Colors";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.screenContainer}>
-      <HomeScreen />
-    </SafeAreaView>
+    <LinearGradient
+      style={styles.screenContainer}
+      colors={[Colors.Background, Colors.Third]}
+    >
+      <ImageBackground
+        style={{ flex: 1 }}
+        resizeMode="cover"
+        imageStyle={{ opacity: 0.2 }}
+        source={require("./assets/images/naruto-shippuden.jpg")}
+      >
+        <HomeScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   screenContainer: {
     flex: 1,
-    backgroundColor: Colors.Background,
   },
 });
